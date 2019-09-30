6a1b4a8130eb067395af37be0d35e59727e723c5
Nghia <tannghianguyen03@gmail.com>
2019-09-30 20:35:39 +0700

    Big update Labs
    Format directory

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+  $("#requirement").click(function(){
+    $("#requirement-content").fadeIn("slow");
+  });
+  $("#hihi").click(function(){
+  	$("#hahaha").hide();
+
+  });
+ 
+});
